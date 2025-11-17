3107a0e93ff6686e90626fa6e9ac9cadff7ddf0e
JDnyanal <dnyanalj@gmail.com>
2025-11-17 19:59:03 +0530

    ready to deploy frontend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,7 @@
-require('dotenv').config();
-const express = require('express');
+// require('dotenv').config();
+import 'dotenv/config';
+import express from 'express';
+// const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 
